b66cb0236ec757011e49a4b8b155e66bca02aa2c
YazminOlivares <146601100+YazminOlivares@users.noreply.github.com>
2024-08-28 20:34:40 -0700

    Create Codigo.js

---
+++ Codigo.js
@@ -0,0 +1,5 @@
+const nombres = ["Tania", "Hannia", "Mariana", "Yazmin"];
+let nombre = "Pepe";
+
+if (nombres.includes(oracion)) console.log("El nombre que coincide con el arreglo es: " + nombre);
+else console.log("No hay coincidencias");
